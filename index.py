97cea4a2cbdd6d5ea4e4a8f98a28d1f9a24728c6
Кривошеев Дмитрий Владимирович <dvkrivosheev@sberbank.ru>
2021-02-04 18:27:29 +0300

    story_mode

--- index.py
+++ index.py
@@ -19,6 +19,7 @@ def handler(event, context):
     new_session = event['session']['new']
     user_location = event['session'].get('location')
     appId = event['session']['application'].get('application_id', None)
+    print('>>>event: ', event, appId)
 
     # state
     appState = event['state']['application']
@@ -38,9 +39,30 @@ def handler(event, context):
         geo_asked = sessionState.get('geo_asked', False)
         story_mode = sessionState.get('story_mode')
 
-        # приветствие
-        if new_session: 
-            return n.welcome(state=sessionState)
+        print('Обработка новой сессии')
+        if new_session==True: 
+            if story_mode==True or appState.get('step'):
+                return n.continue_game(sessionState)
+            else:
+                return n.welcome(state=sessionState)
+
+
+        if context=='continue_game':
+            if 'YANDEX.CONFIRM' in intents:
+                return person(event, appState.get('step'), 'kupol')
+            if 'YANDEX.REJECT' in intents:
+                return n.clear_app_state(appState, sessionState)       
+
+        if context=='welcome':
+            print('YANDEX.CONFIRM' in intents, )
+            if 'YANDEX.CONFIRM' in intents:
+                # запрос геолокации
+                if user_location is None and geo_asked==False:
+                    return n.ask_geo(state=sessionState)
+                if user_location is not None:
+                    return n.how_far_from_kremlin(sState=sessionState, aState=appState, user_location=user_location)
+            if 'YANDEX.REJECT' in intents:
+                    return n.bye()
 
         # запрос геолокации
         if user_location is None and geo_asked==False:
@@ -48,7 +70,7 @@ def handler(event, context):
 
         # начало. где находится пользоваетль
         if context=='ask_geo':
-            return n.how_far_from_kremlin(state=sessionState, user_location=user_location)
+            return n.how_far_from_kremlin(sState=sessionState, aState=appState, user_location=user_location)
 
         if context=='within_kremlin':
             return n.say('Начать экскурсию')
@@ -57,18 +79,20 @@ def handler(event, context):
             return n.say('Подойти к воротам')
 
         if context=='somewhere':
-            return n.say('Начать экскурсию или выйти')
+            if 'YANDEX.CONFIRM' in intents:
+                return person(event, appState.get('step'), 'kupol')
+            if 'YANDEX.REJECT' in intents:
+                return n.bye()
 
-
-
-
-
-
-        if 'YANDEX.CONFIRM' in intents:
-            if context=='welcome': return n.say('Переход к запросу навигации')
-
-        if 'YANDEX.REJECT' in intents:
-            if context=='welcome': return n.say('Выход из квеста')
+        # Если есть данные в appState
+        # if story_mode==True:
+        #     if context=='somewhere':
+        #         if 'YANDEX.CONFIRM' in intents:
+        #             return person(event, appState.get('step'), 'kupol')
+        #         if 'YANDEX.REJECT' in intents:
+        #             return n.bye()
+        #     else:
+        #         return person(event, appState.get('step'), 'kupol')
 
         if 'help' in intents:
             return n.say_help()
--- navigation.py
+++ navigation.py
@@ -4,8 +4,20 @@ from sights import sights
 import phrases as ph
 
 # developer
-# def clear_user_state(state):
-  
+def clear_app_state(aState, sState):
+  for key in aState:
+    aState[key]='null'
+  sState['context'] = 'clear_app_state'
+  return make_only_response(
+    text='appState clear',
+  )
+
+
+def bye():
+  return make_only_response(
+    text='Пока-пока',
+    end_session=True
+  )
 
 
 def welcome(state):
@@ -36,6 +48,13 @@ def ask_geo(state):
     directives = True,
   )
 
+def continue_game(state):
+  state['context'] = 'continue_game'; 
+  return make_only_response(
+    text = 'У вас есть сохранённый прогресс. Продолжить?',
+    buttons=ph.hi['buttons']
+  )
+
 def fallback(command):
   txt='Вы молвили {}. Команда не распознана. Поробуйте ещё раз'.format(command)
   return make_only_response(
@@ -43,40 +62,41 @@ def fallback(command):
   )
 
 # with location
-def how_far_from_kremlin(state, user_location):
+def how_far_from_kremlin(aState, sState, user_location):
   if user_location is not None and user_location['accuracy'] < 100:
       target = sights['one_k_year']
       distance = get_distance_to_object(user_location, target['location'])
       print('Расстояние до {name} составялет {distance}'.format(name=target['name'], distance=distance))
     
       if distance < 300:
-        return within_kremlin(state) 
+        return within_kremlin(aState, sState) 
       if 300 <= distance < 500: 
-        return around_kremlin(state)
+        return around_kremlin(aState, sState)
       if distance > 500:
-        return somewhere(state)
+        return somewhere(aState, sState)
       
 
-def within_kremlin(state):
+def within_kremlin(aState, sState):
   txt = 'Вы на территории Новгодоского Кремля. Рассказать про Кремль?'
-  state['context'] = 'within_kremlin'
+  sState['context'] = 'within_kremlin'
   return make_only_response(
     text=txt,
     buttons=ph.hi['buttons']
   )
 
-def around_kremlin(state):
+def around_kremlin(aState, sState):
   txt = 'Вы рядом с Новгородским Кремлём. Подойдите к Кремлёвским воротам и скажите "Я возле ворот"'
-  state['context'] = 'around_kremlin'
+  sState['context'] = 'around_kremlin'
   return make_only_response(
     text=txt,
     buttons=[{ 'title': "Я возле ворот", 'hide': True }]
   )
 
-def somewhere(state):
+def somewhere(aState, sState):
   txt = 'Вы очень далеко от Новгородского Кремля. Продолжить в режиме повествования?'
-  state['context'] = 'somewhere'
-  state['story_mode'] = True
+  sState['context'] = 'somewhere'
+  sState['story_mode'] = True
+  aState['step'] = 0
   return make_only_response(
     text=txt,
     buttons=ph.hi['buttons']
@@ -101,5 +121,20 @@ def get_distance_to_object(user_location, target_location):
 
   return distance
 
+# story
 
-  
+def begin(state):
+  txt = 'Жил был Сдако... Рассказать про купол?'
+  state['context'] = 'begin'
+  return make_only_response(
+    text=txt,
+    buttons=ph.hi['buttons']
+  )
+
+def one_k_year_story(state):
+  txt = 'Купол этот не простой...Идём к Кащею?'
+  state['context'] = 'one_k_year_story'
+  return make_only_response(
+    text=txt,
+    buttons=ph.hi['buttons']
+  )
